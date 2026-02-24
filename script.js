d8c734a57f57b766b637b51f8d289a7b56a56897
Tushar jha <tusharjha1008@gmail.com>
2026-02-24 15:13:36 +0530

    Tushar

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Hello World!");
+document.write("Hello World!");
+/*some importent notes:
+1 null is a value. 
+2 undefined means a value is not assigned to a variable.
+3 value names are case sensitive.
+4 
